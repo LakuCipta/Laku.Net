946822882843d915bbf14fb645a01c94ec7a2ee8
Dede Soeleman <soeleman@gmail.com>
2013-11-11 18:19:18 +0700

    Pre Solution

---
+++ scr/AssemblyInfoCommon.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyCompany("LakuSoft")]
+[assembly: AssemblyCopyright("Copyright © LakuSoft 2013")]
+[assembly: AssemblyTrademark("LakuSoft")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyCulture("")]
